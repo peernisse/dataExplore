283716c7fbb9842c7905e9a70a8e4bdc80afbcb8
peernisse <peernisse@gmail.com>
2018-06-26 13:20:27 -0600

    Added stat summary table, changed from tableOutput to dataTableOutput. Moved plots to functions in helpers.R

--- app.R
+++ app.R
@@ -1,6 +1,10 @@
 #-----------------On Load Component---------------------------
+#Libraries
 library(shiny)
 library(tidyverse)
+
+#Source functions
+source('helpers.R')
 
 #-----------------UI Component--------------------------------
 ui <- fluidPage(
@@ -20,14 +24,28 @@ ui <- fluidPage(
               
               uiOutput('choose_locs'),
               
+              uiOutput('choose_dates'),
+              actionButton('clrDates','Reset',width='60px')
               
-              dateRangeInput("dates", label = h3("Select Date range"))
+              
               
         ),
       
       # plot preview and data export tools
       mainPanel(
-         tableOutput('plot.data')
+         
+              tabsetPanel(type = "tabs",
+                          tabPanel("Table", tags$div(dataTableOutput('tblData'),style="height=600px")),
+                          tabPanel("Explore Plots", 
+                                   fluidRow(
+                                           column(6,plotOutput("timeSeriesPlot")),
+                                           column(6,plotOutput("boxPlot"))
+                                           )
+                                ),
+                          tabPanel("Multiple Plots","Coming Soon"),
+                          tabPanel("Stats", dataTableOutput('statsData'))
+              )
+                    
       )
    )
 )
@@ -43,11 +61,10 @@ server <- function(input, output) {
                         return(NULL)
                 
                 tbl <- read.csv(inFile$datapath, header=TRUE, stringsAsFactors = FALSE)
+                tbl$LOG_DATE<-as.POSIXct(strptime(tbl$LOG_DATE,format="%d-%b-%y"))
                 
                 return(tbl)
         })
-        
-        
         
         
         output$choose_locs<-renderUI({
@@ -60,11 +77,64 @@ server <- function(input, output) {
                                    selected = locs)
         })
         
-        output$plot.data <- renderTable({
+        output$choose_dates<-renderUI({
+                if(is.null(pData()))
+                        return()
+                minDate<-min(pData()$LOG_DATE)
+                maxDate<-max(pData()$LOG_DATE)
+                
+                dateRangeInput('dtRng',"Choose Date Range",
+                               start = minDate,
+                               end = maxDate,
+                               format = 'dd-M-yyyy'
+                                
+                               )
+        })
+        
+        output$tblData <- renderDataTable({
                 if(is.null(input$locids))
                         return()
-                fData<-filter(pData(),LOC_ID %in% input$locids)
+                
+                fData<-pData() %>% filter(LOC_ID %in% input$locids,
+                                          LOG_DATE >= input$dtRng[1] & LOG_DATE<=input$dtRng[2]) %>% 
+                        arrange(LOC_ID,LOG_DATE)
+                fData$LOG_DATE<-as.character(fData$LOG_DATE)
+                
                 return(fData)
+        })
+        
+        output$timeSeriesPlot <- renderPlot({
+                if(is.null(input$locids))
+                        return()
+                tsData<-as.data.frame(filter(pData(),LOC_ID %in% input$locids,
+                                             LOG_DATE >= input$dtRng[1] & LOG_DATE<=input$dtRng[2]))
+                tsp<-tsPlot(tsData,tsData$LOG_DATE,tsData$WATER_ELEV)
+                return(tsp)
+        })
+        
+        output$boxPlot <- renderPlot({
+                if(is.null(input$locids))
+                        return()
+                bxData<-as.data.frame(filter(pData(),LOC_ID %in% input$locids,
+                                             LOG_DATE >= input$dtRng[1] & LOG_DATE<=input$dtRng[2]))
+                bxp<-bxPlot(bxData,bxData$LOC_ID,bxData$WATER_ELEV)
+                return(bxp)
+        })
+        
+        output$statsData <- renderDataTable({
+                pData() %>% filter(LOC_ID %in% input$locids,
+                                   LOG_DATE >= input$dtRng[1] & LOG_DATE<=input$dtRng[2]) %>% 
+                        group_by(LOC_ID) %>% 
+                        summarise(`Number of Observations`=length(WATER_ELEV),
+                                  `Minimum Date`=min(LOG_DATE),
+                                  `Maximum Date`=max(LOG_DATE),
+                                  Minimum=min(WATER_ELEV),
+                                  Maximum=max(WATER_ELEV),
+                                  `First Quartile`=quantile(WATER_ELEV,0.25),
+                                  `Third Quartile`=quantile(WATER_ELEV,0.75),
+                                  Average=mean(WATER_ELEV),
+                                  `Standard Deviaton`=sd(WATER_ELEV),
+                                  Variance=var(WATER_ELEV))
         })
         
 }
