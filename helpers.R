03bf153297c0ab540eb5384a21e5e075a6920d0c
peernisse <peernisse@gmail.com>
2018-06-27 11:44:03 -0600

    Added functionality to filter by parameter. Plots changed for coloring by locations and faceting by parameter. Began units fixing case statement

--- helpers.R
+++ helpers.R
@@ -1,39 +1,71 @@
 #This script holds helper functions for the data exploration app
 
-#Plots
-
-tsPlot<-function(data,x,y){
+#Functions----------------------------------
+#Fix units cases
+fixUnits<-function(x){
+        df<-x
+        units<-df$Units
+        unitsOut<-case_when(
+                units == 'MG/L' ~ 'mg/l',
+                units == 'mg/l' ~ 'mg/l',
+                units == 'mg/L' ~ 'mg/l',
+                units == 'UG/L' ~ 'ug/l',
+                units == 'ug/l' ~ 'ug/l',
+                units == 'ug/L' ~ 'ug/l',
+                units == 'MG/KG' ~ 'mg/kg',
+                units == 'mg/kg' ~ 'mg/kg'
+        )
         
-        g<-ggplot(data,aes(x=x,y=y, color=LOC_ID))+
+        return(unitsOut)
+}
+
+#Plot functionss--------------------------------
+
+tsPlot<-function(data,x,y,units=NULL){
+        tsdat<-data
+        tsdat$Parameter<-paste0(tsdat$Parameter," (",tsdat$Units,")")
+        g<-ggplot(tsdat,aes(x=x,y=y, color=Location))+
                 geom_point()+
+                facet_wrap(~Parameter,scales="free")+
                 theme(legend.position = "bottom", legend.title = element_blank())+
-                labs(x="Date",y="Groundwater Elevation (meters amsl)",title="Groundwater Elevation Time Series")
+                labs(x="Date",y="Value",title="Time Series")
         
 }
 
 bxPlot<-function(data,x,y){
         
-        g<-ggplot(data,aes(x=x,y=y, fill=LOC_ID))+
+        bxdat<-data
+        bxdat$Parameter<-paste0(bxdat$Parameter," (",bxdat$Units,")")
+        g<-ggplot(bxdat,aes(x=x,y=y, fill=Location))+
                 geom_boxplot()+
+                facet_wrap(~Parameter, scales="free")+
                 theme(legend.position = "bottom", legend.title = element_blank())+
-                labs(x="Location",y="Groundwater Elevation (meters amsl)",title="Groundwater Elevation Boxplots")
+                labs(x="Location",y="Value",title="Boxplots")
         
 }
 
 
 qPlot<-function(data){
-        g<-ggplot(data,aes(sample=WATER_ELEV))+
+        
+        qdat<-data
+        qdat$Parameter<-paste0(qdat$Parameter," (",qdat$Units,")")
+        g<-ggplot(qdat,aes(sample=Value,color=Location))+
                 geom_qq()+
-                facet_wrap(~LOC_ID,scales="free")+
-                labs(x="Theoretical Distribution(normal)",y="Water Elevation (meters amsl)",title="Groundwater Elevation Distribution (quantile plot)")
+                facet_wrap(~Parameter,scales="free")+
+                theme(legend.position = "bottom", legend.title = element_blank())+
+                labs(x="Theoretical Distribution(normal)",y="Value",title="Distribution (quantile plot)")
         
 }
 
 hPlot<-function(data){
-        g<-ggplot(data,aes(x=WATER_ELEV))+
-                geom_histogram()+
-                facet_wrap(~LOC_ID,scales="free")+
-                labs(x="Water Elevation Bins (meters amsl)",y="Count",title="Groundwater Elevation Distribution (histogram)")
+        
+        hdat<-data
+        hdat$Parameter<-paste0(hdat$Parameter," (",hdat$Units,")")
+        g<-ggplot(hdat,aes(x=Value,fill=Location))+
+                geom_histogram(alpha=0.5)+
+                facet_wrap(~Parameter,scales="free")+
+                theme(legend.position = "bottom", legend.title = element_blank())+
+                labs(x="Water Elevation Bins (meters amsl)",y="Count",title="Distribution (histogram)")
 }
 
 
